0ed725f74373ee60b49ca3075289c6f4760512bc
Rufina Litfullina <litllefoot777@gmail.com>
2025-07-19 14:01:34 +0300

    3_hover effect+btn functions

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ async function getAllProducts() {
   });
   const data = await res.json();
   const allProducts = data.products;
+  //console.log(allProducts);
   return allProducts;
 }
 
@@ -47,16 +48,52 @@ async function addNext20Products() {
     newProductSpace.classList.add("goods__item");
     newProductSpace.innerHTML = `
   <article class="goods__body">
-    <img src="" alt="product" class="goods__img" width="150" height="150" loading="lazy">
-    <span class="goods__rating">4.9</span>
-    <span class="goods__title">Платье летнее длинное</span>
-    <span class="goods__price">$499.00</span>
+    <img src="${nextTopProducts[i].images[0]}" alt="product" class="goods__img" width="150" height="150" loading="lazy">
+    <span class="goods__rating">${nextTopProducts[i].rating}</span>
+    <span class="goods__title">${nextTopProducts[i].title}</span>
+    <span class="goods__price">${nextTopProducts[i].price}$</span>
   </article>
 `;
     document.querySelector(".goods__list").appendChild(newProductSpace);
   }
 }
-
 document
   .querySelector(".product__main__btn-add")
   .addEventListener("click", addNext20Products);
+
+// Функции пролистывания фото при наведении
+
+document.querySelector(".goods__list").onmouseout = (e) => {
+  if (e.target.classList.contains("goods__img")) {
+    let arr = [...e.target.src];
+    arr[arr.length - 6] = 1;
+    e.target.src = arr.join("");
+  }
+};
+
+async function isImageAvailable(url) {
+  try {
+    const res = await fetch(url, {
+      method: "GET",
+    });
+    return res.ok;
+  } catch {
+    return false;
+  }
+}
+
+document
+  .querySelector(".goods__list")
+  .addEventListener("mouseover", async (e) => {
+    if (e.target.className === "goods__img") {
+      const firstImage = e.target.src;
+      const secondImage = e.target.src.replace("1.webp", "2.webp");
+
+      const checkImage = await isImageAvailable(secondImage);
+
+      if (checkImage) {
+        e.target.dataset.firstImage = firstImage;
+        e.target.src = secondImage;
+      }
+    }
+  });
